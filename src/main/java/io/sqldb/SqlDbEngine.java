761c307f9eaa1d3321faa03b7f69c317b70c384e
Mateusz <mateuszg18@gmail.com>
2019-05-28 13:31:40 +0200

    line

--- src/main/java/io/sqldb/SqlDbEngine.java
+++ src/main/java/io/sqldb/SqlDbEngine.java
@@ -8,7 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class SqlDbEngine implements DbEngine {
     private final String url;
     private final String user;
